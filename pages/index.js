feb12034a9d0a6bc4ed7028d9c074d4a4e1fe57f
Jiri Kozel <jirik@users.noreply.github.com>
2022-10-26 16:24:41 +0200

    Add AboutDialog

--- pages/index.js
+++ pages/index.js
@@ -5,24 +5,39 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import HelpIcon from '@mui/icons-material/Help';
+import AboutDialog from "../component/AboutDialog";
 
 export default function ButtonAppBar() {
+  const [aboutOpen, setAboutOpen] = React.useState(false);
+
+  const handleAboutClick = () => {
+    setAboutOpen(true);
+  };
+
+  const handleAboutClose = () => {
+    setAboutOpen(false);
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} component="a" href="/">
-            Enjoy Pomootskey
-          </Typography>
-          <IconButton
-            size="large"
-            color="inherit"
-            aria-label="menu"
-          >
-            <HelpIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-    </Box>
+      <>
+        <Box sx={{flexGrow: 1}}>
+          <AppBar position="static">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{flexGrow: 1}} component="a" href="/">
+                Enjoy Pomootskey
+              </Typography>
+              <IconButton
+                  size="large"
+                  color="inherit"
+                  aria-label="menu"
+                  onClick={handleAboutClick}
+              >
+                <HelpIcon/>
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <AboutDialog open={aboutOpen} handleClose={handleAboutClose} />
+      </>
   );
 }
