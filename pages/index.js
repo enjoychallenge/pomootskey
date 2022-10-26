54776dcb1d9668a7eb0e453b178ba0a397cef7ea
Jiri Kozel <jirik@users.noreply.github.com>
2022-10-26 14:41:31 +0200

    Label is link to home page

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function ButtonAppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} component="a" href="/">
             Enjoy Pomootskey
           </Typography>
           <IconButton
