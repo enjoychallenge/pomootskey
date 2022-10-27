6063eeb1e518a9b75954d82436e5e524548f3608
Jiri Kozel <jirik@users.noreply.github.com>
2022-10-27 15:48:04 +0200

    Stop using image optimization

--- next.config.js
+++ next.config.js
@@ -7,4 +7,10 @@ module.exports = withPWA({
   eslint: {
     dirs: ['app', 'component', 'features', 'pages'],
   },
+
+  // Because we use static pages, we need to disable image optimization.
+  // https://stackoverflow.com/questions/65487914/error-image-optimization-using-next-js-default-loader-is-not-compatible-with-n
+  images: {
+    unoptimized: true,
+  },
 })
