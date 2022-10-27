1e0f21e8ed231e886cec77af67a399d126d8da41
Jiri Kozel <jirik@users.noreply.github.com>
2022-10-27 17:25:22 +0200

    Encapsulate AppBar

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,15 @@
 import * as React from 'react'
-import AppBar from '@mui/material/AppBar'
+import AppBar from '../component/AppBar'
 import Box from '@mui/material/Box'
-import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
 import HelpIcon from '@mui/icons-material/Help'
-import AboutDialog from '../component/AboutDialog'
 import styles from '../styles/index.module.css'
 
 export default function ButtonAppBar() {
-  const [aboutOpen, setAboutOpen] = React.useState(false)
-
-  const handleAboutClick = () => {
-    setAboutOpen(true)
-  }
-
-  const handleAboutClose = () => {
-    setAboutOpen(false)
-  }
-
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography
-              variant="h6"
-              sx={{ flexGrow: 1 }}
-              component="a"
-              href="/"
-            >
-              Enjoy Pomootskey
-            </Typography>
-            <IconButton
-              size="large"
-              color="inherit"
-              aria-label="menu"
-              onClick={handleAboutClick}
-            >
-              <HelpIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
+        <AppBar />
         <Box
           component="main"
           className={styles.buttons}
@@ -61,7 +29,6 @@ export default function ButtonAppBar() {
           </div>
         </Box>
       </Box>
-      <AboutDialog open={aboutOpen} handleClose={handleAboutClose} />
     </>
   )
 }
