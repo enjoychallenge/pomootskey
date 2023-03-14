d4dc38a5717cfe9f2752e7f9d6947db39274824e
index-git <sileny.index@gmail.com>
2023-03-14 15:18:39 +0100

    Show emtpy results line

---
+++ component/Placeholder.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import { InputBase } from '@mui/material'
+import input_styles from '../styles/common/input.module.scss'
+
+export default function Placeholder() {
+  return (
+    <InputBase
+      hiddenLabel
+      multiline
+      fullWidth
+      value=""
+      readOnly="true"
+      variant="filled"
+      size="small"
+      className={input_styles.text_input}
+      placeholder={'Zde se po zadání vstupu objeví řešení'}
+    />
+  )
+}
