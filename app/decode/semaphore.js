f685084ff399665cabbab248fd98152a128a1ac3
index-git <sileny.index@gmail.com>
2023-03-22 09:40:28 +0100

    Decode semaphore

---
+++ app/decode/semaphore.js
@@ -0,0 +1,37 @@
+const codeTable = {
+  a: new Set([1, 2]),
+  b: new Set([1, 3]),
+  c: new Set([1, 4]),
+  d: new Set([1, 5]),
+  e: new Set([1, 6]),
+  f: new Set([1, 7]),
+  g: new Set([1, 8]),
+  h: new Set([2, 3]),
+  i: new Set([2, 3]),
+  j: new Set([5, 7]),
+  k: new Set([2, 5]),
+  l: new Set([2, 6]),
+  m: new Set([2, 7]),
+  n: new Set([2, 8]),
+  o: new Set([3, 4]),
+  p: new Set([3, 5]),
+  q: new Set([3, 6]),
+  r: new Set([3, 7]),
+  s: new Set([3, 8]),
+  t: new Set([4, 5]),
+  u: new Set([4, 6]),
+  v: new Set([5, 8]),
+  w: new Set([6, 7]),
+  x: new Set([6, 7]),
+  y: new Set([4, 7]),
+  z: new Set([7, 8]),
+}
+
+export function decode(selected) {
+  const areSetsEqual = (a, b) =>
+    a.size === b.size && [...a].every((value) => b.has(value))
+  const decodedPair = Object.entries(codeTable).find(([_, value]) =>
+    areSetsEqual(selected, value)
+  )
+  return decodedPair ? decodedPair[0] : String.fromCharCode(10734)
+}
