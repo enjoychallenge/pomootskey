7e2e5ef5fbf4db7b7ce6e677f54b308fe242e387
index-git <sileny.index@gmail.com>
2023-03-29 13:46:09 +0200

    Create permute function

--- app/decode/common.js
+++ app/decode/common.js
@@ -6,3 +6,29 @@ export function decodeBySet(setSelected, decodeTable) {
   )
   return decodedPair ? decodedPair[0] : String.fromCharCode(10734)
 }
+
+// https://stackoverflow.com/a/37580979
+export function permute(permutation) {
+  const length = permutation.length,
+    result = [permutation.slice()],
+    c = new Array(length).fill(0)
+  let i = 1,
+    k,
+    p
+
+  while (i < length) {
+    if (c[i] < i) {
+      k = i % 2 && c[i]
+      p = permutation[i]
+      permutation[i] = permutation[k]
+      permutation[k] = p
+      ++c[i]
+      i = 1
+      result.push(permutation.slice())
+    } else {
+      c[i] = 0
+      ++i
+    }
+  }
+  return result
+}
