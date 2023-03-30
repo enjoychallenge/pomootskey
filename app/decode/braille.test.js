0c89b35352d29f4f5eeea55209a154fbe9c44a4b
index-git <sileny.index@gmail.com>
2023-03-30 16:44:25 +0200

    Add braille alternative results

--- app/decode/braille.test.js
+++ app/decode/braille.test.js
@@ -1,4 +1,4 @@
-import { columnsToRows, decode, toUtf } from './braille'
+import { columnsToRows, decode, rowsToColumns, toUtf } from './braille'
 
 test('basic braille decode', () => {
   const testSelected = new Set([1])
@@ -12,6 +12,30 @@ test('basic braille to utf char', () => {
 
 describe('columns to rows', () => {
   it.each([
+    {
+      byColumns: new Set([1]),
+      expByRows: new Set([1]),
+    },
+    {
+      byColumns: new Set([2]),
+      expByRows: new Set([3]),
+    },
+    {
+      byColumns: new Set([3]),
+      expByRows: new Set([5]),
+    },
+    {
+      byColumns: new Set([4]),
+      expByRows: new Set([2]),
+    },
+    {
+      byColumns: new Set([5]),
+      expByRows: new Set([4]),
+    },
+    {
+      byColumns: new Set([6]),
+      expByRows: new Set([6]),
+    },
     {
       byColumns: new Set([1, 2, 3]),
       expByRows: new Set([1, 3, 5]),
@@ -27,5 +51,49 @@ describe('columns to rows', () => {
   ])('columns to rows test', ({ byColumns, expByRows }) => {
     const result = columnsToRows(byColumns)
     expect(result).toEqual(expByRows)
+  })
+})
+
+describe('rows to columns', () => {
+  it.each([
+    {
+      byRows: new Set([1]),
+      expByColumns: new Set([1]),
+    },
+    {
+      byRows: new Set([2]),
+      expByColumns: new Set([4]),
+    },
+    {
+      byRows: new Set([3]),
+      expByColumns: new Set([2]),
+    },
+    {
+      byRows: new Set([4]),
+      expByColumns: new Set([5]),
+    },
+    {
+      byRows: new Set([5]),
+      expByColumns: new Set([3]),
+    },
+    {
+      byRows: new Set([6]),
+      expByColumns: new Set([6]),
+    },
+    {
+      byRows: new Set([1, 2, 3]),
+      expByColumns: new Set([1, 2, 4]),
+    },
+    {
+      byRows: new Set([1, 2, 4, 5]),
+      expByColumns: new Set([1, 3, 4, 5]),
+    },
+    {
+      byRows: new Set([2, 4]),
+      expByColumns: new Set([4, 5]),
+    },
+  ])('rows to columns test', ({ byRows, expByColumns }) => {
+    const result = rowsToColumns(byRows)
+    expect(result).toEqual(expByColumns)
   })
 })
