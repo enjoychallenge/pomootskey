fbe94156a9f998fccefda1799ed4ef5095318ed7
index-git <sileny.index@gmail.com>
2023-04-07 08:37:40 +0200

    Order results by number of decoded unknowns

--- app/decode/commont.test.js
+++ app/decode/commont.test.js
@@ -1,4 +1,9 @@
-import { permute, alternativePermutations } from './common'
+import {
+  permute,
+  alternativePermutations,
+  scoreResult,
+  PartTypes,
+} from './common'
 
 describe('permute', () => {
   it.each([
@@ -40,5 +45,24 @@ describe('alternativePermutations', () => {
     },
   ])('alternativePermutations', ({ input, expResult }) => {
     expect(alternativePermutations(input)).toEqual(expResult)
+  })
+})
+
+describe('scoreResult', () => {
+  it.each([
+    {
+      input: [],
+      expResult: 0,
+    },
+    {
+      input: [{ type: PartTypes.unknown }],
+      expResult: 1,
+    },
+    {
+      input: [{ type: PartTypes.separator }, { type: PartTypes.char }],
+      expResult: 0,
+    },
+  ])('scoreResult', ({ input, expResult }) => {
+    expect(scoreResult(input)).toEqual(expResult)
   })
 })
