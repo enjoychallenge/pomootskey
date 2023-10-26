9b7ebe9cfb002828b68ffad818e328eff194c1cf
Jiri Kozel <jirik@users.noreply.github.com>
2023-10-26 11:22:58 +0200

    Show Morse output synced with input

--- component/MorseResultBox.js
+++ component/MorseResultBox.js
@@ -1,12 +1,28 @@
 import Typography from '@mui/material/Typography'
 import * as React from 'react'
 import Box from '@mui/material/Box'
+import morse_styles from '../styles/morse.module.scss'
 
-export default function MorseResultBox({ label, message }) {
+export default function MorseResultBox({ label, inputItems }) {
+  const partsJsx = inputItems.map((item, idx) => {
+    return (
+      <Box key={idx} className={morse_styles.result_item}>
+        <Box className={morse_styles.result_output_char}>
+          {item.output?.char || ''}
+        </Box>
+        <Box
+          className={morse_styles.result_input_char}
+          sx={{ backgroundColor: 'background.paper' }}
+        >
+          {item.input}
+        </Box>
+      </Box>
+    )
+  })
   return (
-    <Box sx={{ backgroundColor: 'background.paper' }}>
+    <>
       <Typography sx={{ color: 'result.label' }}>{label}</Typography>
-      <Typography>{message}</Typography>
-    </Box>
+      <Box>{partsJsx}</Box>
+    </>
   )
 }
