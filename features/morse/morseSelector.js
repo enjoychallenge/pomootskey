5131d4733350e4f9271cd842f05983e6bbb613c7
Jiri Kozel <jirik@users.noreply.github.com>
2023-10-31 16:32:06 +0100

    Convert messageToInputItems to selector

--- features/morse/morseSelector.js
+++ features/morse/morseSelector.js
@@ -1,1 +1,45 @@
-export const getInput = (state) => state.morse.input
+import { decode } from '../../app/decode/morse'
+import { OutputCharTypes } from '../../component/MorseResultBox'
+import { PartTypes } from '../../app/decode/common'
+import { createSelector } from '@reduxjs/toolkit'
+
+const getInput = (state) => state.morse.input
+
+export const getInputItems = createSelector([getInput], (input) => {
+  const inputItems = []
+
+  decode(input).forEach((msgPart) => {
+    let outputChar = null
+    let outputCharType = OutputCharTypes.unknown
+    if (msgPart.char) {
+      outputChar = msgPart.char.toUpperCase()
+      outputCharType = OutputCharTypes.known
+    } else if (msgPart.type === PartTypes.unknown) {
+      outputChar = `?`
+      outputCharType = OutputCharTypes.unknown
+    } else if (
+      msgPart.type === PartTypes.separator &&
+      msgPart.string.length < 4
+    ) {
+      outputCharType = OutputCharTypes.known
+    }
+    inputItems.push({
+      input: msgPart.string[0],
+      output: {
+        type: outputCharType,
+        showJoiner: [PartTypes.char, PartTypes.unknown].includes(msgPart.type),
+        char: outputChar,
+      },
+    })
+    inputItems.push(
+      ...msgPart.string
+        .split('')
+        .slice(1)
+        .map((msgChar) => ({
+          input: msgChar,
+        }))
+    )
+  })
+
+  return inputItems
+})
---
+++ features/morse/morseSelector.test.js
@@ -0,0 +1,110 @@
+import * as slctr from './morseSelector'
+
+describe('selector getInputItems', () => {
+  test.each([
+    {
+      testCaseId: 'two known letters',
+      inputMessage: '../-.',
+      expResult: [
+        {
+          input: '.',
+          output: { type: 'known', showJoiner: true, char: 'I' },
+        },
+        {
+          input: '.',
+        },
+        {
+          input: '/',
+          output: { type: 'known', showJoiner: false, char: null },
+        },
+        {
+          input: '-',
+          output: { type: 'known', showJoiner: true, char: 'N' },
+        },
+        {
+          input: '.',
+        },
+      ],
+    },
+    {
+      testCaseId: 'unknown letter',
+      inputMessage: '.-.-',
+      expResult: [
+        {
+          input: '.',
+          output: { type: 'unknown', showJoiner: true, char: '?' },
+        },
+        {
+          input: '-',
+        },
+        {
+          input: '.',
+        },
+        {
+          input: '-',
+        },
+      ],
+    },
+    {
+      testCaseId: '3 separators',
+      inputMessage: '///',
+      expResult: [
+        {
+          input: '/',
+          output: { type: 'known', showJoiner: false, char: null },
+        },
+        {
+          input: '/',
+        },
+        {
+          input: '/',
+        },
+      ],
+    },
+    {
+      testCaseId: '4 separators',
+      inputMessage: '////',
+      expResult: [
+        {
+          input: '/',
+          output: { type: 'unknown', showJoiner: false, char: null },
+        },
+        {
+          input: '/',
+        },
+        {
+          input: '/',
+        },
+        {
+          input: '/',
+        },
+      ],
+    },
+    {
+      testCaseId: 'undecodable characters and dot',
+      inputMessage: 'abc.',
+      expResult: [
+        {
+          input: 'a',
+          output: { type: 'unknown', showJoiner: false, char: null },
+        },
+        {
+          input: 'b',
+        },
+        {
+          input: 'c',
+        },
+        {
+          input: '.',
+          output: { type: 'known', showJoiner: true, char: 'E' },
+        },
+      ],
+    },
+  ])(
+    'should return correct inputItems for $testCaseId',
+    ({ testCaseId, inputMessage, expResult }) => {
+      const inputItems = slctr.getInputItems({ morse: { input: inputMessage } })
+      expect(inputItems).toEqual(expResult)
+    }
+  )
+})
