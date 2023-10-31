73d31c5043d10daa5e18266dbaa5e3209830e5d6
Jiri Kozel <jirik@users.noreply.github.com>
2023-10-31 08:57:36 +0100

    Move getJoinerClass and test it

--- features/morse/util.test.js
+++ features/morse/util.test.js
@@ -1,4 +1,5 @@
-import { messageToInputItems } from './util'
+import { messageToInputItems, getJoinerClass } from './util'
+import morse_styles from '../../styles/morse.module.scss'
 
 describe('messageToInputItems', () => {
   test.each([
@@ -105,6 +106,53 @@ describe('messageToInputItems', () => {
     ({ testCaseId, inputMessage, expResult }) => {
       const inputItems = messageToInputItems(inputMessage)
       expect(inputItems).toEqual(expResult)
+    }
+  )
+})
+
+describe('getJoinerClass', () => {
+  test.each([
+    {
+      showJoiner: false,
+      isStartItem: true,
+      isEndItem: true,
+      expResult: morse_styles.result_input_char_joiner_hidden,
+    },
+    {
+      showJoiner: false,
+      isStartItem: false,
+      isEndItem: false,
+      expResult: morse_styles.result_input_char_joiner_hidden,
+    },
+    {
+      showJoiner: true,
+      isStartItem: true,
+      isEndItem: true,
+      expResult: morse_styles.result_input_char_joiner,
+    },
+    {
+      showJoiner: true,
+      isStartItem: false,
+      isEndItem: false,
+      expResult: morse_styles.result_input_char_joiner_middle,
+    },
+    {
+      showJoiner: true,
+      isStartItem: true,
+      isEndItem: false,
+      expResult: morse_styles.result_input_char_joiner_start,
+    },
+    {
+      showJoiner: true,
+      isStartItem: false,
+      isEndItem: true,
+      expResult: morse_styles.result_input_char_joiner_end,
+    },
+  ])(
+    'should return correct CSS class name for showJoiner=$showJoiner isStartItem=$isStartItem isEndItem=$isEndItem',
+    ({ showJoiner, isStartItem, isEndItem, expResult }) => {
+      const className = getJoinerClass(showJoiner, isStartItem, isEndItem)
+      expect(className).toEqual(expResult)
     }
   )
 })
