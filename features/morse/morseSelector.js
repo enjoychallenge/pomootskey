c03ee33ce280ffde2baf2d0a8c73852782721fa6
Jiri Kozel <jirik@users.noreply.github.com>
2023-11-01 08:59:38 +0100

    Move joiner logic to selector

--- features/morse/morseSelector.js
+++ features/morse/morseSelector.js
@@ -9,39 +9,61 @@ export const OutputCharTypes = {
   unknown: 'unknown',
 }
 
+export const JoinerTypes = {
+  hidden: 'hidden',
+  start: 'start',
+  end: 'end',
+  middle: 'middle',
+  single: 'single',
+}
+
 export const getInputItems = createSelector([getInput], (input) => {
   const inputItems = []
 
   decode(input).forEach((msgPart) => {
     let outputChar = null
     let outputCharType = OutputCharTypes.unknown
+    let firstJoiner =
+      msgPart.string.length === 1 ? JoinerTypes.single : JoinerTypes.start
     if (msgPart.char) {
       outputChar = msgPart.char.toUpperCase()
       outputCharType = OutputCharTypes.known
     } else if (msgPart.type === PartTypes.unknown) {
       outputChar = `?`
       outputCharType = OutputCharTypes.unknown
-    } else if (
-      msgPart.type === PartTypes.separator &&
-      msgPart.string.length < 4
-    ) {
-      outputCharType = OutputCharTypes.known
+    } else if (msgPart.type === PartTypes.separator) {
+      firstJoiner = JoinerTypes.hidden
+      if (msgPart.string.length < 4) {
+        outputCharType = OutputCharTypes.known
+      }
+    } else {
+      firstJoiner = JoinerTypes.hidden
     }
     inputItems.push({
       input: msgPart.string[0],
       output: {
         type: outputCharType,
-        showJoiner: [PartTypes.char, PartTypes.unknown].includes(msgPart.type),
         char: outputChar,
       },
+      joiner: firstJoiner,
     })
     inputItems.push(
       ...msgPart.string
         .split('')
         .slice(1)
-        .map((msgChar) => ({
-          input: msgChar,
-        }))
+        .map((msgChar, charIdx) => {
+          let joiner = firstJoiner
+          if (joiner !== JoinerTypes.hidden) {
+            joiner =
+              charIdx < msgPart.string.length - 2
+                ? JoinerTypes.middle
+                : JoinerTypes.end
+          }
+          return {
+            input: msgChar,
+            joiner,
+          }
+        })
     )
   })
 
