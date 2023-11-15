031deeeb7cc0d5b3b4b9213474c94f83469b8d9a
Jiri Kozel <jirik@users.noreply.github.com>
2023-11-15 13:51:24 +0100

    Change getAllResults to selector

--- pages/morse.js
+++ pages/morse.js
@@ -4,9 +4,8 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import layout_styles from '../styles/common/layout.module.scss'
 import morse_styles from '../styles/morse.module.scss'
-import { alternativePermutations } from '../app/decode/common'
 import Button from '@mui/material/Button'
-import { decode, rearrange, MorseCharsToShow } from '../app/decode/morse'
+import { MorseCharsToShow } from '../app/decode/morse'
 import { getResultBoxes } from '../app/results'
 import LongPressButton from '../component/LongPressButton'
 import MorseResultBox from '../component/MorseResultBox'
@@ -27,38 +26,6 @@ import { ArrowForward, ArrowBack, Backspace } from '@mui/icons-material'
 import { ActionButtons } from '../features/morse/morseSelector'
 import { Dialog } from '@mui/material'
 import DialogTitle from '@mui/material/DialogTitle'
-
-const allResults = (message) => {
-  const baseCharOrder = '-./'
-  const alternativeCharOrders = alternativePermutations(baseCharOrder.split(''))
-  const decodedVariants = [
-    {
-      label: 'Základní řešení',
-      message: message,
-    },
-  ]
-    .concat(
-      alternativeCharOrders.map((altCharOrder) => {
-        return {
-          label:
-            'Alternativní řešení ‒●/  ⇒  ' +
-            altCharOrder.reduce(
-              (accum, current) => accum + MorseCharsToShow[current],
-              ''
-            ),
-          message: rearrange(message, altCharOrder.join('')),
-        }
-      })
-    )
-    .map((variant) => {
-      return {
-        label: variant.label,
-        decoded: variant.message.length ? decode(variant.message) : [],
-      }
-    })
-
-  return getResultBoxes(decodedVariants)
-}
 
 const MorseButton = ({ char, onClick, preselected }) => {
   const memoOnClick = useCallback(() => {
@@ -127,7 +94,6 @@ export default function MorsePage() {
   const cursorType = useAppSelector(slctr.getCursorType)
   const actionsButtons = useAppSelector(slctr.getInputActionButtons)
   const morseButtons = useAppSelector(slctr.getMorseButtons)
-  const input = useAppSelector(slctr.getInput)
 
   const actionButtonsJsx = actionsButtons.map(({ type, disabled }, idx) => {
     switch (type) {
@@ -214,7 +180,7 @@ export default function MorsePage() {
             <Dialog onClose={onVariantDialogClose} open={isVariantDialogOpen}>
               <DialogTitle>Kliknutím vyber alternativní variantu</DialogTitle>
               <Box className={layout_styles.result_cases_overview}>
-                {allResults(input)}
+                {getResultBoxes(useAppSelector(slctr.getAllResults))}
               </Box>
             </Dialog>
           </Box>
