b0312ebf087d5d8e80fa8b7cd49dd40478cbd119
Jiri Kozel <jirik@users.noreply.github.com>
2023-11-15 14:28:35 +0100

    Add alternative to state, enable to choose it

--- component/ResultBox.js
+++ component/ResultBox.js
@@ -1,10 +1,23 @@
 import Typography from '@mui/material/Typography'
 import * as React from 'react'
 import Box from '@mui/material/Box'
+import { useCallback } from 'react'
 
-export default function ResultBox({ label, message }) {
+export default function ResultBox({
+  label,
+  message,
+  idx,
+  onVariantClick = null,
+}) {
+  const memoOnVariantClick = useCallback(() => {
+    onVariantClick && onVariantClick(label, idx)
+  }, [onVariantClick, label, idx])
+
   return (
-    <Box sx={{ backgroundColor: 'background.paper' }}>
+    <Box
+      sx={{ backgroundColor: 'background.paper' }}
+      onClick={memoOnVariantClick}
+    >
       <Typography sx={{ color: 'result.label' }}>{label}</Typography>
       <Typography>{message}</Typography>
     </Box>
