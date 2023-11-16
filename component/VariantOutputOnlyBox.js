50f4f06a783ddd18e3c47c0999d8a75136b5cce1
Jiri Kozel <jirik@users.noreply.github.com>
2023-11-16 09:47:40 +0100

    Change cursor on variant selection

--- component/VariantOutputOnlyBox.js
+++ component/VariantOutputOnlyBox.js
@@ -2,6 +2,7 @@ import Typography from '@mui/material/Typography'
 import * as React from 'react'
 import Box from '@mui/material/Box'
 import { useCallback } from 'react'
+import general_styles from '../styles/common/general.module.scss'
 
 export default function VariantOutputOnlyBox({
   label,
@@ -13,10 +14,12 @@ export default function VariantOutputOnlyBox({
     onVariantClick && onVariantClick(label, idx)
   }, [onVariantClick, label, idx])
 
+  const className = onVariantClick ? general_styles.clickable : null
   return (
     <Box
       sx={{ backgroundColor: 'background.paper' }}
       onClick={memoOnVariantClick}
+      className={className}
     >
       <Typography sx={{ color: 'result.label' }}>{label}</Typography>
       <Typography>{message}</Typography>
