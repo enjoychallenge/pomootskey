33c2bcfcb7eca13a9cea46691f2ae98c5a49a070
Jiri Kozel <jirik@users.noreply.github.com>
2023-11-16 09:32:33 +0100

    Rename ResultBox to VariantOutputOnlyBox

--- component/VariantOutputOnlyBox.js
+++ component/VariantOutputOnlyBox.js
@@ -3,7 +3,7 @@ import * as React from 'react'
 import Box from '@mui/material/Box'
 import { useCallback } from 'react'
 
-export default function ResultBox({
+export default function VariantOutputOnlyBox({
   label,
   message,
   idx,
