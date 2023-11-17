5dcf92f6e81391aa67e289ce290eaba7ee73eb8e
Jiri Kozel <jirik@users.noreply.github.com>
2023-11-17 10:42:51 +0100

    Encapsulate getInputItems, move it to util.js with JoinerTypes

---
+++ features/morse/util.js
@@ -0,0 +1,53 @@
+import { decode } from '../../app/decode/morse'
+import { getOutputChar, PartTypeToOutputCharType } from '../../app/results'
+import { PartTypes } from '../../app/decode/common'
+
+export const JoinerTypes = {
+  hidden: 'hidden',
+  start: 'start',
+  end: 'end',
+  middle: 'middle',
+  single: 'single',
+}
+
+export const getInputItems = (input) => {
+  const inputItems = []
+
+  decode(input).forEach((msgPart) => {
+    const outputChar = getOutputChar(msgPart)
+    const outputCharType = PartTypeToOutputCharType[msgPart.type]
+    let firstJoiner =
+      msgPart.input.length === 1 ? JoinerTypes.single : JoinerTypes.start
+    if ([PartTypes.undecodable, PartTypes.separator].includes(msgPart.type)) {
+      firstJoiner = JoinerTypes.hidden
+    }
+    inputItems.push({
+      input: msgPart.input[0],
+      output: {
+        type: outputCharType,
+        char: outputChar,
+      },
+      joiner: firstJoiner,
+    })
+    inputItems.push(
+      ...msgPart.input
+        .split('')
+        .slice(1)
+        .map((msgChar, charIdx) => {
+          let joiner = firstJoiner
+          if (joiner !== JoinerTypes.hidden) {
+            joiner =
+              charIdx < msgPart.input.length - 2
+                ? JoinerTypes.middle
+                : JoinerTypes.end
+          }
+          return {
+            input: msgChar,
+            joiner,
+          }
+        })
+    )
+  })
+
+  return inputItems
+}
