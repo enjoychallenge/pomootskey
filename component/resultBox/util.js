08211c52bf8abd4925492c6842074616f85a28e9
index-git <sileny.index@gmail.com>
2023-11-23 13:13:01 +0100

    Add backspace button to braille page

--- component/resultBox/util.js
+++ component/resultBox/util.js
@@ -1,6 +1,18 @@
 import { CursorTypes } from '../../app/results'
 import { ArrowTypes } from '../../features/morse/morseSlice'
 
+const resetSwiping = (state) => {
+  if ('autoSend' in state) {
+    state.autoSend = true
+  }
+  if ('isSwiping' in state) {
+    state.isSwiping = false
+  }
+  if ('isFocusing' in state) {
+    state.isFocusing = false
+  }
+}
+
 export const arrowMove = (state, direction) => {
   if (direction === ArrowTypes.right && state.cursorIdx < state.input.length) {
     if (state.cursorType === CursorTypes.insert) {
@@ -21,13 +33,31 @@ export const arrowMove = (state, direction) => {
     }
   }
 
-  if ('autoSend' in state) {
-    state.autoSend = true
+  resetSwiping(state)
+}
+
+export const backspace = (state) => {
+  if (state.cursorType === CursorTypes.insert && state.cursorIdx > 0) {
+    state.input = state.input
+      .slice(0, state.cursorIdx - 1)
+      .concat(state.input.slice(state.cursorIdx))
+    state.cursorIdx -= 1
+  } else if (state.cursorType === CursorTypes.edit) {
+    state.input = state.input
+      .slice(0, state.cursorIdx)
+      .concat(state.input.slice(state.cursorIdx + 1))
+    state.cursorType = CursorTypes.insert
   }
-  if ('isSwiping' in state) {
-    state.isSwiping = false
+
+  resetSwiping(state)
+}
+
+export const longBackspace = (state) => {
+  if (state.cursorType === CursorTypes.insert && state.cursorIdx > 0) {
+    state.input = state.input.slice(state.cursorIdx)
+  } else if (state.cursorType === CursorTypes.edit) {
+    state.input = state.input.slice(state.cursorIdx + 1)
   }
-  if ('isFocusing' in state) {
-    state.isFocusing = false
-  }
+  state.cursorType = CursorTypes.insert
+  state.cursorIdx = 0
 }
