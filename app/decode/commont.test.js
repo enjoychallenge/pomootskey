68ef5d42380332b8e8e4d687c066a01c61d879cc
index-git <sileny.index@gmail.com>
2024-12-04 11:44:34 +0100

    Add few test utils

--- app/decode/commont.test.js
+++ app/decode/commont.test.js
@@ -61,3 +61,105 @@ describe('scoreResult', () => {
     expect(scoreResult(input)).toEqual(expResult)
   })
 })
+
+function charToPartType(character) {
+  switch (character) {
+    case '?':
+      return PartTypes.unknown
+    case ' ':
+      return PartTypes.separator
+    default:
+      return PartTypes.char
+  }
+}
+
+describe('charToPartType', () => {
+  it.each([
+    {
+      input: '?',
+      expResult: PartTypes.unknown,
+    },
+    {
+      input: ' ',
+      expResult: PartTypes.separator,
+    },
+    {
+      input: 'a',
+      expResult: PartTypes.char,
+    },
+  ])('charToPartType $input to $expResult', ({ input, expResult }) => {
+    expect(charToPartType(input)).toEqual(expResult)
+  })
+})
+
+function solutionToParts(solution) {
+  return solution.split('').map((character) => {
+    const type = charToPartType(character)
+    const result = {
+      type: type,
+    }
+    if (type === PartTypes.char) {
+      result['char'] = character
+    }
+    return result
+  })
+}
+
+describe('solutionToParts', () => {
+  it.each([
+    {
+      input: '?',
+      expResult: [
+        {
+          type: PartTypes.unknown,
+        },
+      ],
+    },
+    {
+      input: ' ',
+      expResult: [
+        {
+          type: PartTypes.separator,
+        },
+      ],
+    },
+    {
+      input: 'a',
+      expResult: [
+        {
+          type: PartTypes.char,
+          char: 'a',
+        },
+      ],
+    },
+    {
+      input: 'abs? d',
+      expResult: [
+        {
+          type: PartTypes.char,
+          char: 'a',
+        },
+        {
+          type: PartTypes.char,
+          char: 'b',
+        },
+        {
+          type: PartTypes.char,
+          char: 's',
+        },
+        {
+          type: PartTypes.unknown,
+        },
+        {
+          type: PartTypes.separator,
+        },
+        {
+          type: PartTypes.char,
+          char: 'd',
+        },
+      ],
+    },
+  ])('solutionToParts $input to $expResult', ({ input, expResult }) => {
+    expect(solutionToParts(input)).toEqual(expResult)
+  })
+})
