22ca5b8f4dc02781197458c3de261e0e023f9faf
index-git <sileny.index@gmail.com>
2024-12-11 13:14:06 +0100

    Unify binary and ternary pages

--- features/ternary/ternarySlice.js
+++ features/ternary/ternarySlice.js
@@ -2,9 +2,7 @@ import { createSlice } from '@reduxjs/toolkit'
 import { numberSystemsSlice } from '../common/numberSystemsSlice'
 import { chars } from '../../app/decode/ternary'
 
-export const ternarySlice = createSlice(
-  numberSystemsSlice('ternary', chars)
-)
+export const ternarySlice = createSlice(numberSystemsSlice('ternary', chars))
 
 export const {
   onNumberButtonClick,
