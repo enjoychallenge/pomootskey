1ca0c27a9758d438bdffe0105bba89fd2cf658b2
index-git <sileny.index@gmail.com>
2025-04-13 08:32:54 +0200

    Use quickSort to sort variants and do paging out of the worker

--- app/decode/common.js
+++ app/decode/common.js
@@ -120,3 +120,41 @@ export function scoreResult(result) {
   )
   return score
 }
+
+function quickSortSwap(arr, el1, el2) {
+  let swapedElem = arr[el1]
+  arr[el1] = arr[el2]
+  arr[el2] = swapedElem
+}
+
+export function quickSortByScore(arr, leftPos, rightPos, arrLength) {
+  let initialLeftPos = leftPos
+  let initialRightPos = rightPos
+  let direction = true
+  let pivot = rightPos
+  while (leftPos - rightPos < 0) {
+    if (direction) {
+      if (arr[pivot].score < arr[leftPos].score) {
+        quickSortSwap(arr, pivot, leftPos)
+        pivot = leftPos
+        rightPos--
+        direction = !direction
+      } else leftPos++
+    } else {
+      if (arr[pivot].score <= arr[rightPos].score) {
+        rightPos--
+      } else {
+        quickSortSwap(arr, pivot, rightPos)
+        leftPos++
+        pivot = rightPos
+        direction = !direction
+      }
+    }
+  }
+  if (pivot - 1 > initialLeftPos) {
+    quickSortByScore(arr, initialLeftPos, pivot - 1, arrLength)
+  }
+  if (pivot + 1 < initialRightPos) {
+    quickSortByScore(arr, pivot + 1, initialRightPos, arrLength)
+  }
+}
