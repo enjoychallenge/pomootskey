4bcc2d906b158a487a0565a0d5e05dcb2222d03e
index-git <sileny.index@gmail.com>
2025-05-23 12:09:23 +0200

    WIP: Resize Words page after a mobile keyboard appears

--- pages/_app.js
+++ pages/_app.js
@@ -25,7 +25,7 @@ export default function MyApp({ Component, pageProps }) {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta
           name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no, interactive-widget=resizes-content"
         />
         <meta name="description" content="Description" />
         <meta name="keywords" content="Keywords" />
